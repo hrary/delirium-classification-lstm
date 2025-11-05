3c9a2d8c01604f3cdc5b02ea2ec0e253b677431e
hrary <harrylu068@gmail.com>
2025-11-04 21:31:23 -0500

    Deleted useless main file

--- main.py
+++
@@ -1,6 +0,0 @@
-from models.classificationAlgorithm import ClassificationAlgorithm
-
-if __name__ == '__main__':
-    model = ClassificationAlgorithm()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
